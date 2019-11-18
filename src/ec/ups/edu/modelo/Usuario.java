4fc2f7a4a91f91c64198f541ecdb7a7202c8f976
Domenica Cañizares <Domenica Caizares@Domenica>
2019-11-18 17:20:49 -0500

    Creacion de packages para dividir y organizar las clases

--- src/ec/ups/edu/modelo/Usuario.java
+++ src/ec/ups/edu/modelo/Usuario.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ec.ups.edu;
+package ec.ups.edu.modelo;
 
 /**
  *
  * @author Domenica Cañizares
  */
-public class clase {
+public class Usuario {
     
 }
